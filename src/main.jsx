549bd3d5197e3b2634b36f3330ba786de28439c5
SysifosWeb <sysifosweb@gmail.com>
2024-05-06 23:32:26 -0400

    Crear sistema de Rutas
    https://trello.com/c/6HR1Iz5u/3-crear-sistema-de-rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import ReactDOM from "react-dom/client";
+import Index from "./web/Index";
+import Home from "./web/Pages/Home";
+import Nosotros from "./web/Pages/Nosotros";
+import Contacto from "./web/Pages/Contacto";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "Nosotros",
+        element: <Nosotros />,
+      },
+      {
+        path: "Contacto",
+        element: <Contacto />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);

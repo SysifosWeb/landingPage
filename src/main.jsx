fc2d87d673626ed855999609329f92f6ec86e5af
SysifosWeb <sysifosweb@gmail.com>
2024-07-25 14:39:51 -0400

    cambio de ruta css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,5 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import ReactDOM from "react-dom/client";
 import Index from "./Index";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Index />);

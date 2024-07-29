430432d801b27e2d4f6698c19885e382ccee9b81
SysifosWeb <sysifosweb@gmail.com>
2024-07-29 18:08:56 -0400

    sin modo estricto

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Index from './Index'
 import './main.css'
-ReactDOM.createRoot(document.getElementById(`root`)).render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById(`root`)).render(<Index />)

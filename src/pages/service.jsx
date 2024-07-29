4c247a20a3823240241f1141f06abaa731084b88
SysifosWeb <sysifosweb@gmail.com>
2024-07-29 17:20:29 -0400

    sections in mobile

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -12,7 +12,7 @@ const Service = () => {
         <b>Nuestros Servicios</b>
       </h2>
 
-      <section className={serv.movile}>
+      <section className={`${serv.movile} d-none d-sm-none d-md-block`}>
         <div className="d-flex justify-content-center justify-content-md-between align-items-center container px-5 pb-5 pb-md-0 px-md-0">
           <article className="d-flex justify-content-center align-items-end d-none d-md-block">
             <img src={movileImg} alt="" className={serv.movileImg} />
@@ -28,7 +28,15 @@ const Service = () => {
         </div>
       </section>
 
-      <section className={serv.web}>
+      {/* mobile section */}
+      <section
+        className={`d-flex justify-content-center align-items-center d-sm-block d-md-none ${serv.movile_m}`}
+      >
+        <h3 className="text-center">Desarrollo de Aplicaciones Móviles</h3>
+      </section>
+      {/* fin mobile section */}
+
+      <section className={`${serv.web} d-none d-sm-none d-md-block`}>
         <div className="d-flex justify-content-md-between align-items-center container py-5">
           <article className="d-flex flex-column justify-content-center align-items-center align-items-md-start px-5">
             <h3 className="text-white text-center text-md-start mt-5">Desarrollo Web</h3>
@@ -37,13 +45,21 @@ const Service = () => {
               en línea de tu negocio.
             </p>
           </article>
-          <article className="d-flex align-items-center justify-content-start d-none d-md-block">
+          <article className="d-flex align-items-center justify-content-start ">
             <img src={webImg} alt="" />
           </article>
         </div>
       </section>
 
-      <section className={serv.solution}>
+      {/* mobile section */}
+      <section
+        className={`d-flex justify-content-center align-items-center d-sm-block d-md-none ${serv.web_m}`}
+      >
+        <h3 className="text-center">Desarrollo Web</h3>
+      </section>
+      {/* fin mobile section */}
+
+      <section className={`${serv.solution} d-none d-sm-none d-md-block`}>
         <div className="d-flex justify-content-md-between align-items-center container py-5">
           <article className="d-flex align-items-center justify-content-center d-none d-md-block">
             <img src={solutionImg} alt="" />
@@ -58,12 +74,15 @@ const Service = () => {
         </div>
       </section>
 
+      {/* mobile section */}
       <section
-        className={serv.system}
-        style={{
-          background: `linear-gradient(-90deg,rgb(0, 0, 54)1%,rgba(9,9,121,1)20%,rgba(0,212,255,1))`,
-        }}
+        className={`d-flex justify-content-center align-items-center d-sm-block d-md-none ${serv.solution_m}`}
       >
+        <h3>Soluciones Empresariales</h3>
+      </section>
+      {/* fin mobile section */}
+
+      <section className={`${serv.system} d-none d-sm-none d-md-block`}>
         <div className="d-flex justify-content-md-between align-items-center container">
           <article className="d-flex flex-column justify-content-center align-items-center align-items-md-start px-5 py-5 py-md-0">
             <h3 className="text-white text-sm-center">Integración de Sistemas</h3>
@@ -77,6 +96,14 @@ const Service = () => {
           </article>
         </div>
       </section>
+
+      {/* mobile section */}
+      <section
+        className={`d-flex justify-content-center align-items-center d-sm-block d-md-none ${serv.system_m}`}
+      >
+        <h3>Integración de Sistemas</h3>
+      </section>
+      {/* fin mobile section */}
     </main>
   )
 }

1a8062fd0aa634d9c59296a4ace08c425f88f0d5
SysifosWeb <sysifosweb@gmail.com>
2024-07-29 22:17:42 -0400

    optimizador de imagenes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 10,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 10,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 10,
+      },
+    }),
+  ],
 })

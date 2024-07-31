7b4cbc38f087797762d8fb772992f7ed0901a084
SysifosWeb <sysifosweb@gmail.com>
2024-07-30 22:07:39 -0400

    Prueba de Seo con react-head

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import { HeadProvider } from 'react-head'
 import Index from './Index'
 import './main.css'
-ReactDOM.createRoot(document.getElementById(`root`)).render(<Index />)
+ReactDOM.createRoot(document.getElementById(`root`)).render(
+  <HeadProvider>
+    <Index />
+  </HeadProvider>,
+)

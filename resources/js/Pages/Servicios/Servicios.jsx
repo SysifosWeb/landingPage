9e02e8a220c2f5ab44b0e4e8d3c266a3f4934a7d
SysifosWeb <osman_ahumada@hotmail.com>
2024-10-08 00:06:33 -0300

    avance pobre xD

--- resources/js/Pages/Servicios/Servicios.jsx
+++ resources/js/Pages/Servicios/Servicios.jsx
@@ -1,8 +1,9 @@
 import Header from "./../HeaderPages";
-import Contact from "./../Contact";
+// import Contact from "./../Contact";
 import Footer from "./../Footer";
 import { Head } from "@inertiajs/react";
 import "./../../../css/main.css";
+import service from "./../../../css/servicios.module.css";
 
 const Servicios = () => {
     return (
@@ -12,7 +13,29 @@ const Servicios = () => {
                 <meta name="description" content="pagina de nosotros" />
             </Head>
             <Header title={"Nuestros Servicios"} />
-            <Contact />
+            <main>
+                <section className={service.web} id="paginasWeb">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h1 className="text-center py-4">
+                                    Paginas webs
+                                </h1>
+                                <p className="text-center">
+                                    Posicionamos tu negocio en internet, y te
+                                    acercamos a más clientes!, con un diseño de
+                                    pagina enfocado en lo que ofreces, y con
+                                    compatibilidad en la mayoria de los
+                                    dispositivos del mercado
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="" id="aplicacionesMedida"></section>
+                <section className="" id="appsMobile"></section>
+            </main>
+            {/* <Contact /> */}
             <Footer />
         </>
     );

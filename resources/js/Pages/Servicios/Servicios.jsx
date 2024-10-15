897144e1edd5e784345a9fd13059d14f24bed391
SysifosWeb <osman_ahumada@hotmail.com>
2024-10-15 17:15:36 -0300

    tarea cron 1 vez al mes

--- resources/js/Pages/Servicios/Servicios.jsx
+++ resources/js/Pages/Servicios/Servicios.jsx
@@ -30,6 +30,70 @@ const Servicios = () => {
                                 </p>
                             </div>
                         </div>
+                        <div className="row d-flex align-items-center justify-content-evenly my-5">
+                            <div
+                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
+                            >
+                                <h4>Plan basico</h4>
+                                <p>$1.000.000 clp</p>
+                                <ul className="mt-4">
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                    <li>✔️ Awesome features</li>
+                                </ul>
+                                <button className="btn btn-primary">
+                                    Obtener Plan
+                                </button>
+                            </div>
+                            <div
+                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table} ${service.pro}`}
+                            >
+                                <h4>Plan profesional</h4>
+                                <p>$3.000.000 clp</p>
+                                <ul className="mt-4">
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                </ul>
+                                <div className="d-grid">
+                                    <button
+                                        className="btn btn-light"
+                                        type="button"
+                                    >
+                                        Obtener Plan
+                                    </button>
+                                </div>
+                            </div>
+                            <div
+                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
+                            >
+                                <h4>Plan empresarial</h4>
+                                <p>$10.000.000 clp</p>
+                                <ul className="mt-4">
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                    <li>Awesome features</li>
+                                </ul>
+                                <button className="btn btn-primary">
+                                    Obtener Plan
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </section>
                 <section className="" id="aplicacionesMedida"></section>

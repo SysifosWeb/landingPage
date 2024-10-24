00044c4b5c5de3bbf640dc5e3866fcb9bc493204
SysifosWeb <osman_ahumada@hotmail.com>
2024-10-23 21:34:45 -0300

    sección pagina web - lista

--- resources/js/Pages/Servicios/Servicios.jsx
+++ resources/js/Pages/Servicios/Servicios.jsx
@@ -2,6 +2,7 @@ import Header from "./../HeaderPages";
 // import Contact from "./../Contact";
 import Footer from "./../Footer";
 import { Head } from "@inertiajs/react";
+import { Check } from "@mui/icons-material";
 import "./../../../css/main.css";
 import service from "./../../../css/servicios.module.css";
 
@@ -21,7 +22,9 @@ const Servicios = () => {
                                 <h1 className="text-center py-4">
                                     Paginas webs
                                 </h1>
-                                <p className="text-center">
+                                <p
+                                    className={`text-justify ${service.parrafo}`}
+                                >
                                     Posicionamos tu negocio en internet, y te
                                     acercamos a más clientes!, con un diseño de
                                     pagina enfocado en lo que ofreces, y con
@@ -30,68 +33,156 @@ const Servicios = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="row d-flex align-items-center justify-content-evenly my-5">
-                            <div
-                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
-                            >
-                                <h4>Plan basico</h4>
-                                <p>$1.000.000 clp</p>
-                                <ul className="mt-4">
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                    <li>✔️ Awesome features</li>
-                                </ul>
-                                <button className="btn btn-primary">
-                                    Obtener Plan
-                                </button>
-                            </div>
-                            <div
-                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table} ${service.pro}`}
-                            >
-                                <h4>Plan profesional</h4>
-                                <p>$3.000.000 clp</p>
-                                <ul className="mt-4">
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                </ul>
-                                <div className="d-grid">
-                                    <button
-                                        className="btn btn-light"
-                                        type="button"
-                                    >
+                        <div className="row">
+                            <div className="d-flex d-flex align-items-center justify-content-center my-5">
+                                <div
+                                    className={`col-lg-4 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
+                                >
+                                    <small>Sitio estático</small>
+                                    <h4>Plan basico</h4>
+                                    <p className={service.price}>185.640</p>
+                                    <small>iva incluido</small>
+                                    <ul className="mt-4 list-group list-group-flush">
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Hosting anual
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Dominio .cl x 1 año
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Correos Corporativos
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Menú de 3 Secciones
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Certificado SSL
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />1 mes de
+                                            Manteción
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Autoadministrable
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            1gb de Almacenamiento
+                                        </li>
+                                    </ul>
+                                    <button className="btn btn-primary mt-4">
                                         Obtener Plan
                                     </button>
                                 </div>
-                            </div>
-                            <div
-                                className={`col-lg-3 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
-                            >
-                                <h4>Plan empresarial</h4>
-                                <p>$10.000.000 clp</p>
-                                <ul className="mt-4">
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                    <li>Awesome features</li>
-                                </ul>
-                                <button className="btn btn-primary">
-                                    Obtener Plan
-                                </button>
+                                <div
+                                    className={`col-lg-4 d-flex flex-column align-items-center justy-content-center ${service.price_table} ${service.pro}`}
+                                >
+                                    <small>Sitio dinámico</small>
+                                    <h4>Plan emprendedor</h4>
+                                    <p className={service.price}>297.500</p>
+                                    <small>iva incluido</small>
+                                    <ul className="mt-4 list-group list-group-flush">
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Hosting anual
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Dominio .cl x 1 año
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Correos Corporativos
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Menú de 5 Secciones
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Certificado SSL
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />2 meses de
+                                            Manteción
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Autoadministrable
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            2gb de Almacenamiento
+                                        </li>
+                                        <li className="list-group-item text-white bg-transparent">
+                                            <Check className="me-2" />
+                                            Botón de whatsapp
+                                        </li>
+                                    </ul>
+                                    <div className="d-grid">
+                                        <button
+                                            className="btn btn-light mt-4"
+                                            type="button"
+                                        >
+                                            Obtener Plan
+                                        </button>
+                                    </div>
+                                </div>
+                                <div
+                                    className={`col-lg-4 d-flex flex-column align-items-center justy-content-center ${service.price_table}`}
+                                >
+                                    <small>Sitio dinámico</small>
+                                    <h4>Plan premium</h4>
+                                    <p className={service.price}>476.000</p>
+                                    <small>iva incluido</small>
+                                    <ul className="mt-4 list-group list-group-flush">
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Hosting anual
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Dominio .cl x 2 años
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Correos Corporativos
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Menú de 10 Secciones
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Certificado SSL
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />3 meses de
+                                            Manteción
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Autoadministrable
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            3gb de Almacenamiento
+                                        </li>
+                                        <li className="list-group-item">
+                                            <Check className="me-2" />
+                                            Botón de whatsapp
+                                        </li>
+                                    </ul>
+                                    <button className="btn btn-primary mt-4">
+                                        Obtener Plan
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>

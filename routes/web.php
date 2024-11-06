bc079233bd39b3d520b8d3d5426d82bb28cb5741
SysifosWeb <osman_ahumada@hotmail.com>
2024-11-05 22:42:56 -0300

    init

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('Home/Home');
+    return Inertia::render('Home');
 });
-
-Route::get('/nosotros', function () {
-    return inertia('Nosotros/Nosotros');
-});
-
-Route::get('/servicios', function () {
-    return inertia('Servicios/Servicios');
-});
-
-// Route::get('/portafolio', function () {
-//     return inertia('Portafolio/Portafolio');
-// });
-
-// Route::get('/blog', function () {
-//     return inertia('Blog/Blog');
-// });
-
-Route::get('/contacto', function () {
-    return inertia('Contacto/Contacto');
-});
-
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,}),
-        react(),
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
-    resolve : {
-        alias: {
-            '@': '/resources/js',
-        }
-    }
 });

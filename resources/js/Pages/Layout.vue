eb772134585d56bfc0571b71975bef69acb8f50d
karen371 <karengodoy325@gmail.com>
2024-11-06 23:40:01 -0300

    Agrega estructura inicial del navbar con enlaces principales

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import Navbar from './Component/Navbar.vue';
 </script>
 
 <template>
   <main>
     <header>
-      <Link href="/">Home</Link>
+      <!-- <Link href="/">Home</Link>
       <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <Link href="/contact">Contact</Link> -->
+      <Navbar/>
     </header>
     <article>
       <slot />

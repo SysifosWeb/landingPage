c82e7723d826a468855b2ff6e9944d92f88aa704
Diego Araya <diego.araya.lobos@gmail.com>
2024-12-01 22:57:11 -0300

    feat: :sparkles: agrega links de paginas y rrss al footer

--- resources/js/Pages/Footer.jsx
+++ resources/js/Pages/Footer.jsx
@@ -1,8 +1,16 @@
 import logo from "./../../img/logo-blanco.webp";
 import indexcss from "./../../css/index.module.css";
-import { Instagram, LocalPhone, MailOutline } from "@mui/icons-material";
+import {
+    Facebook,
+    Instagram,
+    LinkedIn,
+    LocalPhone,
+    MailOutline,
+} from "@mui/icons-material";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function Footer() {
+    const { url } = usePage();
     return (
         <footer>
             <div
@@ -13,20 +21,100 @@ export default function Footer() {
                     <img src={logo} alt="Logo" height="50rem" />
                 </div>
                 <div
-                    className={`d-flex flex-column justify-content-md-start ${indexcss.dir}`}
+                    className={`d-flex flex-column justify-content-md-start mb-4 mb-md-0 ${indexcss.dir}`}
+                >
+                    <div className="mb-3 text-start">Páginas</div>
+                    <div className="d-flex align-items-center mb-3">
+                        <Link
+                            href="/"
+                            aria-current="page"
+                            className={`nav-link
+                                        ${url === "/" ? "active" : ""}
+                                    `}
+                        >
+                            Home
+                        </Link>
+                    </div>
+                    <div className="d-flex align-items-center mb-3">
+                        <Link
+                            href="/nosotros"
+                            aria-current="page"
+                            className={`nav-link
+                                        ${url === "/nosotros" ? "active" : ""}
+                                    `}
+                        >
+                            Nosotros
+                        </Link>
+                    </div>
+                    <div className="d-flex align-items-center mb-3">
+                        <Link
+                            href="/servicios"
+                            aria-current="page"
+                            className={`nav-link
+                                        ${url === "/servicios" ? "active" : ""}
+                                    `}
+                        >
+                            Servicios
+                        </Link>
+                    </div>
+                    <div className="d-flex align-items-center mb-3">
+                        <Link
+                            href="/contacto"
+                            aria-current="page"
+                            className={`nav-link
+                                        ${url === "/contacto" ? "active" : ""}
+                                    `}
+                        >
+                            Contacto
+                        </Link>
+                    </div>
+                </div>
+                <div
+                    className={`d-flex flex-column justify-content-md-start mb-4 mb-md-0 ${indexcss.dir}`}
                 >
                     <div className="mb-3 text-start">Contactanos</div>
                     <div className="d-flex align-items-center mb-3">
-                        <LocalPhone fontSize="large" sx={{ mr: 1 }} />
-                        +56949109970
+                        <a
+                            href="tel:+56949109970"
+                            className={`${indexcss.link}`}
+                        >
+                            <LocalPhone fontSize="large" sx={{ mr: 1 }} />
+                            +56949109970
+                        </a>
                     </div>
                     <div className="d-flex align-items-center mb-3">
                         <MailOutline fontSize="large" sx={{ mr: 1 }} />
                         contacto@sysifosweb.cl
                     </div>
                     <div className="d-flex align-items-center mb-3">
-                        <Instagram fontSize="large" sx={{ mr: 1 }} />
-                        sysifosweb_
+                        <a
+                            href="https://www.instagram.com/sysifosweb_/"
+                            target="_blank"
+                            className={`${indexcss.link}`}
+                        >
+                            <Instagram fontSize="large" sx={{ mr: 1 }} />
+                            sysifosweb_
+                        </a>
+                    </div>
+                    <div className="d-flex align-items-center mb-3">
+                        <a
+                            href="https://www.facebook.com/profile.php?id=61567859694020"
+                            target="_blank"
+                            className={`${indexcss.link}`}
+                        >
+                            <Facebook fontSize="large" sx={{ mr: 1 }} />
+                            Sysifosweb
+                        </a>
+                    </div>
+                    <div className="d-flex align-items-center mb-3">
+                        <a
+                            href="https://www.linkedin.com/company/sysifos-web/"
+                            target="_blank"
+                            className={`${indexcss.link}`}
+                        >
+                            <LinkedIn fontSize="large" sx={{ mr: 1 }} />
+                            Sysifos Web
+                        </a>
                     </div>
                 </div>
             </div>

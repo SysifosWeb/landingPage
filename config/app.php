4fa8c3625345d951fd4d806522623952c54586bd
Diego Araya <diego.araya.lobos@gmail.com>
2025-10-16 20:01:03 -0300

    fix: set Blade public views feature toggle to true

--- config/app.php
+++ config/app.php
@@ -123,6 +123,6 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'use_blade_public_views' => env('USE_BLADE_PUBLIC_VIEWS', true),
+    'use_blade_public_views' => true,
 
 ];

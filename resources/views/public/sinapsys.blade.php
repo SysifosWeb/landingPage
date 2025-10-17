32735451e2cb0ebdea9880b18f826eda2dd4a144
Osman Ahumada <osman_ahumada@hotmail.com>
2025-10-17 19:56:29 -0300

    fix: propiedades og

--- resources/views/public/sinapsys.blade.php
+++ resources/views/public/sinapsys.blade.php
@@ -8,14 +8,14 @@
   <meta name="robots" content="index, follow">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="canonical" href="https://sysifosweb.cl/sinapsys">
-  <meta property="og:title" content="SysifosWeb - Software Developer">
+  <meta property="og:title" content="SysifosWeb | Sinapsys">
   <meta property="og:description" content="Especialistas en desarrollo de software, aplicaciones web y soluciones digitales en Chile. Laravel, Vue.js, React y más. Cotiza tu proyecto gratis.">
   <meta property="og:image" content="https://sysifosweb.cl/storage/logos/logo-simple-min.jpg">
   <meta property="og:image:width" content="600">
   <meta property="og:image:height" content="600">
   <meta property="og:url" content="https://sysifosweb.cl/sinapsys">
   <meta property="og:type" content="website">
-  
+  <meta name="fb:app_id" content="1215450019494398">
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="SysifosWeb - Sinapsys">
   <meta name="twitter:description" content="Especialistas en desarrollo de software, aplicaciones web y soluciones digitales en Chile. Laravel, Vue.js, React y más. Cotiza tu proyecto gratis.">
